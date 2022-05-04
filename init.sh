446a06f9a06be701fe2638aa4a90b4c4c6b7c913
Richard <surichard@users.noreply.github.com>
2022-05-05 06:52:50 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # Created by Richard
 # MAIL:richar.su@163.com
 cat << EOF
@@ -7,11 +8,50 @@ cat << EOF
 +--------------------------------------------------------------+
 +--------------------------by Richard--------------------------+
 EOF
-#set ntp
-yum install epel-release -y
-
-#history time
-if ! grep HISTTIMEFORMAT /etc/profile; then
-    echo 'export HISTTIMEFORMAT="%F %T `whoami` "' >> /etc/profile
+#run check root
+if [ "$UID" != "0" ]; then
+    echo "Please run this script by root"
+    exit 1
 fi
+#### 1.install epel repo
+mod_yum() {
+    if [ -e /etc/yum.repos.d/CentOS-Base.repo ]; then
+        cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.default
+        yum install -y epel-release  && yum clean all && yum makecache && yum -y update
+    fi
+}
+
+#### 4. install software
+install_softwares() {
+    # 1
+    if [ `rpm -qa vim lrzsz wget nmap nc tree curl tcpdump sysstat lsof net-tools ntpdate|wc -l` -lt 13 ]; then
+        yum -y install vim lrzsz wget nmap nc tree curl tcpdump sysstat lsof net-tools ntpdate dos2unix
+    fi
+    # 0
+    yum groups install "Development Tools" -y
+}
+
+#### 7. Configure environment variables
+com_line_set() {
+    if [ `egrep 'TMOUT|HISTSIZE|HISTFILESIZE' /etc/profile|wc -l` ]; then
+        # Set session timeout
+        echo 'export TMOUT=1800' >> /etc/profile
+        # History command output record line number
+        echo 'export HISTSIZE=1000' >> /etc/profile
+        # The total number of records saved by the history command
+        echo 'export HISTFILESIZE=1000' >> /etc/profile
+        # History command output format
+        echo  'export HISTTIMEFORMAT="%F %T `whoami` "' >> /etc/profile
+        source /etc/profile
+    fi
+}
+
+main() {
+    mod_yum
+    install_softwares
+    com_line_set
+}
+
+main
+
 
